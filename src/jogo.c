d7fc7452cf098f9c4fdf039494deefba501aa6d0
dfrduarte <dfrduarte2006@gmail.com>
2025-05-12 16:45:21 +0100

    Acho q está

--- src/jogo.c
+++ src/jogo.c
@@ -310,7 +310,7 @@ void guardar(Tabuleiro* tab, Pilha* pilha, const char* ficheiro) {
 }
 
 // Função para remover uma jogada da pilha (desfazer)
-void desfazer(Tabuleiro* tab, Pilha* pilha) {
+void desfazer(Tabuleiro* tab, Pilha* pilha, int vprintar) {
     if (pilha->topo == -1) {
         printf("Não há jogadas para desfazer!\n");
         return;
@@ -329,7 +329,7 @@ void desfazer(Tabuleiro* tab, Pilha* pilha) {
         // Desfaz apenas a última jogada
         Jogada ultimaJogada = pilha->jogadas[pilha->topo--];
         tab->grelha[ultimaJogada.lin][ultimaJogada.col] = ultimaJogada.anterior;
-        printf("Última jogada desfeita.\n");
+        if (vprintar) printf("Última jogada desfeita.\n");
     }
 }
 
@@ -483,7 +483,7 @@ void resolver(Tabuleiro* tab, Pilha* pilha, int vprintar, int in, int jn) {
 
                 // desfaz tentativa de riscar
                 while (pilha->topo > marcador) {
-                    desfazer(tab, pilha);
+                    desfazer(tab, pilha, 0);
                     pilha->numJogadasR--; // decrementa o numero de jogadas feitas
                 }
 
@@ -501,7 +501,7 @@ void resolver(Tabuleiro* tab, Pilha* pilha, int vprintar, int in, int jn) {
 
                 // desfaz tentativa de branco
                 while (pilha->topo > marcador) {
-                    desfazer(tab, pilha);
+                    desfazer(tab, pilha, 0);
                     pilha->numJogadasR--; // decrementa o numero de jogadas feitas
                 }
 
--- src/main.c
+++ src/main.c
@@ -79,13 +79,14 @@ int main() {
             pilha.resolverConcluido = 0; // para saber quando devo desfazer um bloco ou nao na funcao desfazer
             resolver(tab, &pilha, 0, 0, 0);
             pilha.resolverConcluido = 1;
+            if (!pilha.numJogadasR) printf("O tabuleiro pode ser resolvido!\n");
         }
         else if (acao == 'd') {
             if (!tab) {
                 printf("Tabuleiro não carregado! Tente novamente.\n");
                 continue;
             }
-            desfazer(tab, &pilha);  // Desfaz a última jogada
+            desfazer(tab, &pilha, 1);  // Desfaz a última jogada
         }
         else if (acao == 'b') {
             if (!tab) {
